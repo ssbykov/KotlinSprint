e942db1140bab1e8ffb6b21091a0b3ba6d13e2bb
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-06 12:26:30 +0300

    KS-1-3 wrote a solution to problem 3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("Посадка – $hour:$minute")
+}
