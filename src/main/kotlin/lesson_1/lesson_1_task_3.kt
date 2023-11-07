9d238e045ff2be9bb53bfe2750ee798df557dfbf
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-07 10:54:24 +0300

    KS-1-3 the input data format has been changed

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -2,15 +2,19 @@ package lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    val takeoffTime = "09:07"
+
+    var hour = takeoffTime.split(":")[0]
+    var minute = takeoffTime.split(":")[1]
 
     println(year)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    val landingTime = "10:55"
+
+    hour = landingTime.split(":")[0]
+    minute = landingTime.split(":")[1]
 
     println("Посадка – $hour:$minute")
 }
