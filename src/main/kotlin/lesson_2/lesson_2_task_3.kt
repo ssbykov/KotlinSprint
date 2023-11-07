6cce02dc4dbd6a738b29843f276d5bf2d3e8592b
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-07 11:08:10 +0300

    KS-2-3 the input data format has been changed

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,8 +1,9 @@
 package lesson_2
 
 fun main() {
-    val hours = 9
-    val minutes = 39
+    val arrivalTime = "9:39"
+    val hours = arrivalTime.split(":")[0].toInt()
+    val minutes = arrivalTime.split(":")[1].toInt()
     val travelTime = 457
 
     println("${((hours * 60 + minutes + travelTime) / 60) % 24}:${(minutes + 457) % 60}")
