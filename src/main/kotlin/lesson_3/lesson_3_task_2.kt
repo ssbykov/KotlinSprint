ade55a85dd73531cc33527bcf4744d0b9db62d1e
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-08 18:45:18 +0300

    KS-3-2 name and age output

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var surname = "Андреева"
+    var age = 20
+
+    println("[$surname $name $patronymic, $age]")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("[$surname $name $patronymic, $age]")
+
+}
