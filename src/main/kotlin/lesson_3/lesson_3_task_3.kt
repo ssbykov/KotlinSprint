b51502421fdff852edcc2f22126ee995369fdf85
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-08 18:50:21 +0300

    KS-3-2 a multiplication table has been created

---
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -0,0 +1,17 @@
+package lesson_3
+
+fun main() {
+    val digit = 6
+
+    println("""
+        $digit x 1 = ${digit * 1}
+        $digit x 2 = ${digit * 2}
+        $digit x 3 = ${digit * 3}
+        $digit x 4 = ${digit * 4}
+        $digit x 5 = ${digit * 5}
+        $digit x 6 = ${digit * 6}
+        $digit x 7 = ${digit * 7}
+        $digit x 8 = ${digit * 8}
+        $digit x 9 = ${digit * 9}
+    """.trimIndent())
+}
