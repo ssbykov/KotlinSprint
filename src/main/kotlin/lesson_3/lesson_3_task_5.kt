991e52c6e1ce45e7fe9e16cae10e7b46ad20a694
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-09 10:49:01 +0300

    KS-3-5 optimized parsing

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -2,9 +2,7 @@ package lesson_3
 
 fun main() {
     val move = "D2-D4;0"
-    val number= move.split(";")[1]
-    val from = move.split("-")[0]
-    val to = move.split("-")[1].split(";")[0]
+    val (from, to, number) = move.split("-", ";")
 
     println(from)
     println(to)
