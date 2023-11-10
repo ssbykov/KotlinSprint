8000bd5e270800a74ff063d95ac41a6195443c33
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-10 11:14:15 +0300

    KS-4-5 moved variables to constants

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -1,18 +1,26 @@
 package lesson_4
 
+const val MIN_TEAM_MEMBER = 55
+const val MAX_TEAM_MEMBER = 70
+const val MIN_PROVISIONS = 50
+
 fun main() {
     print("Корабль не имеет повреждений? 1 - имеет/ 0 - не имеет: ")
     val isDamage = readlnOrNull() == "1"
+
     print("Введите количество членов экипажа: ")
     val numberOfCrew = readlnOrNull()?.toInt()
+
     print("Введите количество ящиков провизии: ")
     val quantityOfProvisions = readLine()!!.toInt()
+
     print("Погода благоприятная? 1 - да/ 0 - нет: ")
     val isWeather = readlnOrNull() == "1"
 
     val approved =
-        !isDamage && numberOfCrew in (55..70) && quantityOfProvisions > 50 && isWeather ||
-                isDamage && numberOfCrew == 70 && quantityOfProvisions >= 50 && isWeather
+        (!isDamage && numberOfCrew in (MIN_TEAM_MEMBER..MAX_TEAM_MEMBER)
+                && quantityOfProvisions > MIN_PROVISIONS && isWeather) ||
+                (isDamage && numberOfCrew == MAX_TEAM_MEMBER && quantityOfProvisions >= MIN_PROVISIONS && isWeather)
 
 
     println(approved)
